49029b4cff4809870e0163aa4749a10f0ad7ce31
EvgenyMinin <evgeny.frontend@gmail.com>
2021-06-25 19:21:16 +0300

    added reduxjs toolkit

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ store.subscribe(() => {
   console.log("Store changed!");
 });
 
-store.dispatch(actions.bugAdded("Bug 1"));
-store.dispatch(actions.bugAdded("Bug 2"));
-store.dispatch(actions.bugAdded("Bug 3"));
+store.dispatch(actions.bugAdded({ description: "Bug 1" }));
+store.dispatch(actions.bugAdded({ description: "Bug 2" }));
+store.dispatch(actions.bugAdded({ description: "Bug 3" }));
 
 console.log(store.getState()); // [{id: 1, description: 'Bug 1', resolved: false}]
 
-store.dispatch(actions.bugResolved(1));
+store.dispatch(actions.bugResolved({ id: 1 }));
 
 console.log(store.getState()); // [{id: 1, description: 'Bug 1', resolved: true}]
 
-store.dispatch(actions.bugRemoved(1));
+store.dispatch(actions.bugRemoved({ id: 1 }));
 
 console.log(store.getState()); // []
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,29 +1,14 @@
+import { createAction } from "@reduxjs/toolkit";
+
 // Action types
 const BUG_ADDED = "BUG_ADDED";
 const BUG_REMOVED = "BUG_REMOVED";
 const BUG_RESOLVED = "BUG_RESOLVED";
 
 // Action Creators
-export const bugAdded = (description) => ({
-  type: BUG_ADDED,
-  payload: {
-    description,
-  },
-});
-
-export const bugRemoved = (id) => ({
-  type: BUG_REMOVED,
-  payload: {
-    id,
-  },
-});
-
-export const bugResolved = (id) => ({
-  type: BUG_RESOLVED,
-  payload: {
-    id,
-  },
-});
+export const bugAdded = createAction("bugAdded");
+export const bugResolved = createAction("bugResolved");
+export const bugRemoved = createAction("bugRemoved");
 
 // Reducer
 let lastId = 0;
@@ -33,7 +18,7 @@ export default function reducer(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case BUG_ADDED:
+    case bugAdded.type:
       return [
         ...state,
         {
@@ -42,11 +27,11 @@ export default function reducer(state = initialState, action) {
           resolved: false,
         },
       ];
-    case BUG_RESOLVED:
+    case bugResolved.type:
       return state.map((bug) =>
         bug.id !== payload.id ? bug : { ...bug, resolved: true }
       );
-    case BUG_REMOVED:
+    case bugRemoved.type:
       return state.filter((bug) => bug.id !== payload.id);
     default:
       return state;
