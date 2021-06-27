37a5f50423e19c4665c43b4112145a9c552d6d4b
EvgenyMinin <evgeny.frontend@gmail.com>
2021-06-27 08:09:07 +0300

    added dispatching function

--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
 import configureStore from "./store/configureStore";
-import {
-  bugAdded,
-  bugResolved,
-  bugToUserAssigned,
-  bugsByUserSelector,
-} from "./store/bugs";
-import { userAdded } from "./store/users";
-import { projectAdded } from "./store/projects";
 
 const store = configureStore();
 
-store.subscribe(() => {
-  console.log("Store changed!");
+store.dispatch((dispatch, getState) => {
+  dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
+  console.log(getState());
 });
-
-// store.dispatch(projectAdded({ name: "Project 1" }));
-
-// store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugAdded({ description: "Bug 3" }));
-
-store.dispatch(userAdded({ name: "User 1" }));
-// store.dispatch(userAdded({ name: "User 2" }));
-
-// store.dispatch(bugToUserAssigned({ bugId: 1, userId: 1 }));
-
-// store.dispatch(bugResolved({ id: 1 }));
-
-const bugsByUser = bugsByUserSelector(1)(store.getState());
-
-console.log(`bugsByUser`, bugsByUser)
-
-console.log(store.getState());
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import logger from "./middleware/logger";
+import func from "./middleware/func";
 
 import reducer from "./reducer";
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [logger({ destination: "console" })],
+    middleware: [logger({ destination: "console" }), func],
   });
 }
