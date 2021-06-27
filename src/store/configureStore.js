316ae4c1b63cf8403303c9941ccf29748708ec6f
EvgenyMinin <evgeny.frontend@gmail.com>
2021-06-27 12:33:36 +0300

    added toastify error

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import logger from "./middleware/logger";
 import func from "./middleware/func";
+import toast from "./middleware/toast";
 
 import reducer from "./reducer";
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [logger({ destination: "console" }), func],
+    middleware: [logger({ destination: "console" }), func, toast],
   });
 }
