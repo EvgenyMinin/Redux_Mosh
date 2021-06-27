7b7c4280a0bdfb3c7efcd70c753d5c93ad1d9494
EvgenyMinin <evgeny.frontend@gmail.com>
2021-06-27 13:23:44 +0300

    added api middleware

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import configureStore from "./store/configureStore";
 
 const store = configureStore();
 
-store.dispatch((dispatch, getState) => {
-  dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
-  console.log(getState());
+store.dispatch({
+  type: "apiCallBegan",
+  payload: {
+    url: "/bugs",
+    onSuccess: "bugsReceived",
+    onError: "apiRequestFailed",
+  },
 });
-
-store.dispatch({ type: "error", payload: { message: "An error occurred" } });
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import logger from "./middleware/logger";
 import func from "./middleware/func";
 import toast from "./middleware/toast";
+import api from "./middleware/api";
 
 import reducer from "./reducer";
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [logger({ destination: "console" }), func, toast],
+    middleware: [
+      ...getDefaultMiddleware(),
+      logger({ destination: "console" }),
+      func,
+      toast,
+      api,
+    ],
   });
 }
