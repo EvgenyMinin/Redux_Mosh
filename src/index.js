ec6a05b355531ffca7a4fbe5b2485f4f4bd1f294
EvgenyMinin <evgeny.frontend@gmail.com>
2021-06-27 07:46:56 +0300

    added middleware

--- src/index.js
+++ src/index.js
@@ -14,18 +14,18 @@ store.subscribe(() => {
   console.log("Store changed!");
 });
 
-store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(projectAdded({ name: "Project 1" }));
 
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugAdded({ description: "Bug 3" }));
 
 store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
+// store.dispatch(userAdded({ name: "User 2" }));
 
-store.dispatch(bugToUserAssigned({ bugId: 1, userId: 1 }));
+// store.dispatch(bugToUserAssigned({ bugId: 1, userId: 1 }));
 
-store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(bugResolved({ id: 1 }));
 
 const bugsByUser = bugsByUserSelector(1)(store.getState());
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import logger from "./middleware/logger";
+
 import reducer from "./reducer";
 
 export default function () {
-  return configureStore({ reducer });
+  return configureStore({
+    reducer,
+    middleware: [logger],
+  });
 }
